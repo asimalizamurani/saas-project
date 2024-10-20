68054890ef168eeb1351141a7beff80c1aadc8cd
Asim Ali <asimalizamurani@gmail.com>
2024-10-20 12:57:45 +0500

    creating a saas project using nextjs with clerk

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prismaClientSingleTon = () => {
+    return new PrismaClient()
+}
+
+type prismaClientSingleton = ReturnType<typeof prismaClientSingleTon>;
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+
+const prisma = globalForPrisma.prisma ??  prismaClientSingleTon();
+
+export default prisma
+
+if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
